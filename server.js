0be8352ee0376e7f271e7c4cf45503ef9230aa5d
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-06-12 20:20:09 -0400

    all CRUD actions complete and working on projects

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const ProjectsRouter = require('./projectRouter');
 
 const server = express();
 // routers import
 
 server.use(express.json());
-
 // router use
-
+server.use('/projects', ProjectsRouter);
 server.get('/', (req, res) => {
   res.send(`<h2>Sprint Challenge!</h2>`);
 });
